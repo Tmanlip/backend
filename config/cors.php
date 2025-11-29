896d4a2c2a356b97c6831f30a9c737ee4529c3c8
Tmanlip <tengku03@graduate.utm.my>
2025-11-29 08:56:17 +0800

    Add front-end link tp cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://red-desert-039397a00.2.azurestaticapps.net'],
+    'allowed_origins' => ['http://localhost:3000', 'https://gentle-bush-0c3f5a800.3.azurestaticapps.net'],
     'allowed_headers' => ['*'],
     'supports_credentials' => false,
 ];
