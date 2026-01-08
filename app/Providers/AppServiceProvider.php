cfd8a4859bb3c1ebfff6555169cdf4fa6a2e1195
Tmanlip <tengku03@graduate.utm.my>
2026-01-08 19:56:25 +0800

    Remove redundant

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,52 +1,18 @@
 <?php
 
-    namespace App\Providers;
+namespace App\Providers;
 
-    use Illuminate\Support\ServiceProvider;
-    use Illuminate\Support\Facades\Route;
-    use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-    use Illuminate\Support\Facades\Storage;
-    use League\Flysystem\Filesystem;
-    use Illuminate\Filesystem\FilesystemAdapter;
-    use League\Flysystem\AzureBlobStorage\AzureBlobStorageAdapter;
+use Illuminate\Support\ServiceProvider;
 
-    class AppServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
     {
-        /**
-         * Register any application services.
-         */
-        public function register(): void
-        {
-            //
-        }
-
-        /**
-         * Bootstrap any application services.
-         */
-        public function boot(): void
-        {
-            // Load the routes here directly
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-    Storage::extend('azure', function ($app, $config) {
-
-        $connectionString = "DefaultEndpointsProtocol=https;AccountName={$config['name']};AccountKey={$config['key']};EndpointSuffix=core.windows.net";
-
-        $client = BlobRestProxy::createBlobService($connectionString);
-
-        $adapter = new AzureBlobStorageAdapter(
-            $client,
-            $config['container']
-        );
-
-        return new Filesystem($adapter);
-    });
-
+        //
     }
-        
+
+    public function boot(): void
+    {
+        //
+    }
 }
-
-
