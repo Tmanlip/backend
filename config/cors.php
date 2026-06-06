a108d6911c738a134054a3899e946438daaad7d2
Tmanlip <tengku03@graduate.utm.my>
2026-06-06 11:33:29 +0800

    New front-end url

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins_patterns' => [],
     'allowed_origins' => array_filter(array_map('trim', explode(',', env(
         'CORS_ALLOWED_ORIGINS',
-        'http://localhost:3000,http://localhost:3001,http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174,https://gentle-bush-0c3f5a800.3.azurestaticapps.net'
+        'http://localhost:3000,http://localhost:3001,http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174,https://zealous-plant-0a3d4cf00.7.azurestaticapps.net'
     )))),
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
